13207daffa11c9a693c600fe1ebe8fb22b5ac108
Styrbjörn Nordberg <styris.n@gmail.com>
2023-05-17 15:04:04 +0200

    base part 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { GraphQLClient, ClientContext } from 'graphql-hooks';
+
+const client = new GraphQLClient({
+  url: 'https://graphql.datocms.com',
+  headers: { Authorization: `Bearer ${import.meta.env.VITE_API_KEY}` },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ClientContext.Provider value={client}>
+      <App />
+    </ClientContext.Provider>
+  </React.StrictMode>
+);
