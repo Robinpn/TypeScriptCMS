4b231ff9565fd0211c6ddb9fb2e88cd551ecab16
Styrbjörn Nordberg <styris.n@gmail.com>
2023-05-19 14:02:20 +0200

    added arrow

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -2,13 +2,20 @@ import * as React from 'react';
 import './style.css';
 
 type HeroImage = {
-    imgUrl: string;
-}
+  imgUrl: string;
+};
 
 const Hero = (props: HeroImage): React.ReactElement => {
-    return (
-        <img className='hero-img' src={props.imgUrl} alt="hero image" />
-    )
-}
+  return (
+    <>
+      <section className="hero">
+        <img className="hero-img" src={props.imgUrl} alt="hero image" />
+        <button className="arrow-btn">
+          <p>&#8595;</p>
+        </button>
+      </section>
+    </>
+  );
+};
 
 export default Hero;
