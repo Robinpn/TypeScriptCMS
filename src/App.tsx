11aef4f6c45a91117f494475bf8d5bf10f4a590e
Styrbjörn Nordberg <styris.n@gmail.com>
2023-05-30 14:22:10 +0200

    AAAAAAAA

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import {GetContentQuery} from '../gql/graphql'
 import './App.css';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
-import Menu from './components/Menu/Menu';
 import Pizza from './components/Pizza/Pizza';
 import Footer from './components/Footer/Footer';
 import About from './components/About/About';
@@ -81,21 +80,25 @@ function App() {
     return <p>something went wrong...</p>
   }
 
-  //fetch pizza array
-  const pizzaData: object[] = data.allPizzas;
+  interface ingredient {
+    name: string;
+  }
 
-  //map all pizzas
-  const pizzaArr: any = pizzaData.map((pizza: any) => {
-    return pizza;
-  });
+  type pizza = {
+    name: string;
+    image: {
+      url: string
+    };
+    ingredientList: ingredient[];
+  }
 
   return (
     <>
       <Navbar Title={data.hero?.title || ""} />
       <main>
         <Hero imgUrl={data.hero?.heroImage?.url || ""} />
-        <Menu>
-          {data.allPizzas.map((pizza: any, index: number) => {
+        <section id="menu" className="pizza-section">
+          {data.allPizzas.map((pizza: pizza, index: number) => {
             return (
               <Pizza
                 key={index}
@@ -105,7 +108,7 @@ function App() {
               />
             );
           })}
-        </Menu>
+        </section>
         <About description={data.about?.description || ""} />
       </main>
       <Footer
